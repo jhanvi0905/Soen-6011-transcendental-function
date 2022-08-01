3a6953019f3f3bf082525acdf792f1d28d028d25
jhanvi0905 <arorajhanvi38@gmail.com>
2022-08-01 05:13:46 -0400

    Feat: Added JUnit tests

--- src/test/java/CosInverse/ACosFunctionTest.java
+++ src/test/java/CosInverse/ACosFunctionTest.java
@@ -1,4 +1,33 @@
 package CosInverse;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class ACosFunctionTest {
+    @Test
+    public void test0() {
+        assertEquals(1.5707963267945, ACosFunction.acos(0), 0.0);
+    }
+
+    @Test
+    public void testNegative(){
+        assertEquals(1.87548898, ACosFunction.acos(-0.3), 0.00001);
+    }
+
+    @Test
+    public void testPositive(){
+        assertEquals(1.47062891, ACosFunction.acos(0.1), 0.00001);
+    }
+
+    @Test
+    public void testDegreeResult(){
+        assertEquals(120, ACosFunction.acos(-0.5)*180/Constants.PI, 0.001);
+    }
+
+    @Test
+    public void testOutOfRange(){
+        assertTrue(ACosFunction.acos(3)==-1);
+    }
 }
