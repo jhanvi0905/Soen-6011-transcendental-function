895ee6e9607c4c375402a4294f4136691415f245
jhanvi0905 <arorajhanvi38@gmail.com>
2022-08-03 14:45:55 -0400

    Feat: Fast Arc Cos Algorithm

--- src/main/java/CosInverse/ACosFunction.java
+++ src/main/java/CosInverse/ACosFunction.java
@@ -31,31 +31,18 @@ public class ACosFunction {
      * @return Acos value of the input number.
      */
     public static double acos(double x) {
-        double a, b, c, d=0, e, f;
-        boolean negate = x < 0;
-        if (negate) {
-            x = -x;
-        }
-        if ((x > 1) | (x < -1)) {
-            return -1;
-        }
-        if (x == 1) {
-            return negate ? Constants.PI : 0;
-        }
-        if(x < 1/ Constants.eps){
-            return Constants.PI/2;
-        }
-        a = x < 0.5 ? x*x : (negate ? (1 + x) * 0.5 : (1 - x) * 0.5);
-        b = a * (Constants.b0 + a * (Constants.b1 + a * (Constants.b2 + a * (Constants.b3 + a * (Constants.b4 + a * Constants.b5)))));
-        c = 1 + a * (Constants.c0 + a * (Constants.c1 + a * (Constants.c2 + a * Constants.c3)));
-        if (x < 0.5) {
-            d = x - (Constants.PI_L / 2 - x * (b / c));
-            return negate ? ((Constants.PI) / 2 + d) : (Constants.PI / 2 - d);
-        }
-        e = squareRoot(a);
-        double g = (a - e * e) / (e + e);
-        f = negate ? b / c * e - Constants.PI_L / 2 : b / c * e + g;
-        return 2 * (e + f);
+        double negative_number = x<0?1:0;
+        x = Math.abs(x);
+        double ret = -0.0187293;
+        ret = ret * x;
+        ret = ret + 0.0742610;
+        ret = ret * x;
+        ret = ret - 0.2121144;
+        ret = ret * x;
+        ret = ret + 1.5707288;
+        ret = ret * squareRoot(1.0-x);
+        ret = ret - 2 * negative_number * ret;
+        return negative_number * 3.14159265358979 + ret;
     }
 
     /**
