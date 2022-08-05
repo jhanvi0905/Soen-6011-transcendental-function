c2b4a01e878f0f8c41573b23dc17d8fad3ed426b
jhanvi0905 <arorajhanvi38@gmail.com>
2022-08-05 04:24:38 -0400

    Doc: Added Report in Latex And PDF

--- src/test/java/CosInverse/ACosFunctionTest.java
+++ src/test/java/CosInverse/ACosFunctionTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.InputMismatchException;
+
 /*
  * This class is used to test the functionality of the CosInverse class.
  */
@@ -16,6 +18,22 @@ public class ACosFunctionTest {
     @Test
     public void test0() {
         assertEquals(1.5707963267945, ac.acos(0), 0.0001);
+    }
+
+    /*
+    Checks value at negative 1 boundary.
+     */
+    @Test
+    public void testneg1(){
+        assertEquals(3.14159265358979, ac.acos(-1), 0.0001);
+    }
+
+    /*
+    Checks value at positive 1 boundary.
+     */
+    @Test
+    public void testpos1(){
+        assertEquals(0.0, ac.acos(1), 0.0001);
     }
 
     /*
@@ -56,5 +74,13 @@ public class ACosFunctionTest {
     @Test
     public void testSquareRoot(){
         assertEquals(1.732, ac.squareRoot(3), 0.001);
+    }
+
+    /*
+    Input Not Defined for Mismatch data type.
+     */
+    @Test
+    public void testWrongInput() {
+        assertEquals(-1, ac.acos('s'), 0.0001);
     }
 }
